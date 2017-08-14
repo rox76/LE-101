b1b983bc9a959e5064407adb8e147d801a13eca2
rox76 <ronalguyome@hotmail.fr>
2017-08-14 12:56:48 +0000

    hello

---
+++ J02/ex05/ft_print_com2.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+
+	a = '0';
+	b = '0';
+	c = '0';
+	d = '0';
+	e = 44;
+	f = 32;
+
+	while (a <= '9')
+	{
+		if (d > b)
+		{
+			write(1, &a, 1);
+			write(1, &b, 1);
+			write(1, &f, 1);
+			write(1, &c, 1);
+			write(1, &d, 1);
+			if (a == '9')
+			{
+				if (b != '8')
+				{
+				write(1, &e, 1);
+				write(1, &f, 1);
+				}
+			}
+			else
+			{
+				write(1, &e, 1);
+				write(1, &f, 1);
+			}
+		}
+		if (a == '9')
+		{
+			if (b == '7' || b == '8')
+			{
+				b ++;
+			}
+			else
+			{
+				d ++;
+				if (d > '9')
+				{
+					c ++;
+					d = '0';
+					if (c > '9')
+					{
+						c = '0';
+						b ++;
+						if (b > '9')
+						{
+							a ++;
+							b = '0';
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			d ++;
+			if (d > '9')
+			{
+				c ++;
+				d = '0';
+				if (c > '9')
+				{
+					c = '0';
+					b ++;
+					if (b > '9')
+					{
+						a ++;
+						b = '0';
+					}
+				}
+			}
+		}
+
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	write(1, "\n", 1);
+	return (0);
+}
