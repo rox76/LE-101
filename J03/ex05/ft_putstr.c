816501ad51517dc7d51ef2f75927e9cd1267caa6
rox76 <ronalguyome@hotmail.fr>
2017-08-16 13:32:17 +0000

    ex05 j03 finish apres ce fucking ; de la boucle while X)

--- J03/ex05/ft_putstr.c
+++ J03/ex05/ft_putstr.c
@@ -3,20 +3,23 @@
 void	ft_putstr(char *str)
 {
 	int i;
-	i = 0;
+	i =0 ;
 
-	while (str[i] != '\0');
+	while (*(str + i))
 	{
-		ft_putchar(str[i]);
+		ft_putchar(*(str + i));
 		i ++;
 	}
 }
 
 int	main(void)
 {
+	int i;
 	char *ptr2;
-	
 	ptr2 = "Hello World";
+	i=0;
+	
 	ft_putstr(ptr2);
+	ft_putchar('\n');
 	return (0);
 }
