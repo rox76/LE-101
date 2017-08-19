6e728f809d6de67480b0e13a24eaaed95eb55d94
rox76 <ronalguyome@hotmail.fr>
2017-08-19 14:03:54 +0000

    A revoir, mais compris

---
+++ J03/ex08/ft_atoi.c
@@ -0,0 +1,51 @@
+#include "function.h"
+#include <stdlib.h>
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int len;
+	int nb;
+	int mult;
+
+	i = 0;
+	len = 0;
+	nb = 0;
+	mult = 1;
+
+	while (*(str + len))
+	{
+		len ++;
+	}
+	i = len - 1;
+
+	while (*(str + i))
+	{
+		if (*(str + i) >= '0' && *(str + i) <= '9')
+		{
+			nb += (*(str + i) - 48) * mult;
+		}
+		mult *= 10;
+		len --;
+		i --;
+	}
+
+	if (nb >= 0)
+	{
+		return (nb);
+	}
+	else
+	{
+		return (-nb);
+	}
+}
+int	main(void)
+{
+	int a;
+	char chaine[] = "455";
+	a = atoi (chaine);
+//	a = ft_atoi(chaine);
+	ft_putnbr(a);
+	ft_putchar('\n');
+	return (0);
+}
