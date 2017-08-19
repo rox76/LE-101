1699785c267a5c463223949f0616e62a7838bbc7
rox76 <ronalguyome@hotmail.fr>
2017-08-19 13:18:37 +0000

    MERCI GITHUB HAHAHA

--- J03/ex07/ft_strrev.c
+++ J03/ex07/ft_strrev.c
@@ -2,42 +2,38 @@
 
 char	*ft_strrev(char *str)
 {
-	int cmptr;
-	int i;
-	char *temp;
+	int	counter;
+	int	size_str;
+	char	temp;
 
-	cmptr = i = 0;
+	counter = 0;
+	size_str = 0;
 
-	while (*(str + cmptr))
+	while (*(str + size_str))
 	{
-		cmptr ++;
+		size_str ++;
 	}
-	
+	size_str -= 1;
 
-	while (cmptr >= 0)
+	while (counter < size_str)
 	{
-		*(temp + i) = *(str + cmptr);
-		cmptr --;
-		i ++;
+		temp = *(str +size_str);
+		*(str + size_str) = *(str + counter);
+		*(str + counter) = temp;
+		counter ++;
+		size_str --;
 	}
-	while (cmptr <= i)
-	{
-		*(str + cmptr) = *(temp + cmptr);
-	}
-
-
 	return (str);
 }
 
 int	main(void)
 {
 	char	ptr[] = "Hello wold";
-	char	*ptr2;
 	int i;
 	i = 0;
 
-	ptr2 = ft_strrev(ptr);
-	while (*(ptr + i))
+	ft_strrev(ptr);
+	while (ptr[i] != '\0')
 	{
 		ft_putchar(*(ptr + i));
 		i ++;
