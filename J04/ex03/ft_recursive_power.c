0c6d17263798d5ce1bd90f0eb0571d12ab25318e
rox76 <ronalguyome@hotmail.fr>
2017-08-20 22:29:37 +0000

    C est changeant, il faut que je m habitue

---
+++ J04/ex03/ft_recursive_power.c
@@ -0,0 +1,33 @@
+#include "function.h"
+
+int	ft_recursive_power(int nb, int power)
+{
+	int i;
+	i = nb;
+	if (nb <= 0 || power <= 0)
+	{
+		return (0);
+	}
+
+	else if (power == 1)
+	{
+		return (nb);
+	}
+	else
+	{
+		nb += ft_recursive_power(nb, power - 1);
+		nb *= i;
+		return (nb);
+	}
+}
+
+int	main(void)
+{
+	int *a;
+	int i;
+	i = ft_recursive_power(2, 10);
+	a = &i;
+	ft_putnbr(*a);
+	ft_putchar('\n');
+	return (0);
+}
