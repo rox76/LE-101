af0e818240202a5798e0f1311a02ebdc6ed7a27e
rox76 <ronalguyome@hotmail.fr>
2017-08-21 15:54:08 +0000

    ft_is prime terminee, mais a bien relire

--- J04/ex06/ft_is_prime.c
+++ J04/ex06/ft_is_prime.c
@@ -4,40 +4,27 @@ Un nombre premier est un nombre qui est divisible que par le chiffre 1 ou par lu
 */
 int	ft_is_prime(int nb)
 {
-	int	x;
-	int	y;
+	int i;
 
-	x = 1;
-	y = 1;
-
+	i = 2;
 	if (nb <= 1)
 	{
 		return (0);
 	}
-	else
+	while (i * i <= nb)
 	{
-		while (x <= 9)
+		if (nb % i == 0)
 		{
-			if((nb % x == 0) && x != nb)
-			{
-				while (y <= 9)
-				{
-					if ((nb % y == 0) && y != nb)
-					{
-						return (1);
-					}
-				}
-				return (0);
-			}
-			x ++;
+			return (0);
 		}
-		return (0);
+		i ++;
 	}
+	return (1);
 }
 
 int	main(void)
 {
-	ft_putnbr(ft_is_prime(4));//NOMBRE PREMIER == 2,3,5,7,11,13,17,19,23,29,31,37,41,83...
+	ft_putnbr(ft_is_prime(18));//NOMBRE PREMIER == 2,3,5,7,11,13,17,19,23,29,31,37,41,83...
 	ft_putchar('\n');
 	return (0);
 }
